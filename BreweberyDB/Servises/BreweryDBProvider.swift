70fc126a7427e6a397bf8be5eb373a931f829862
D_Ivan <JamesDeht@gmail.com>
2020-02-01 14:20:09 +0200

    time gambling

--- BreweberyDB/Servises/BreweryDBProvider.swift
+++ BreweberyDB/Servises/BreweryDBProvider.swift
@@ -7,16 +7,56 @@
 //
 
 import Foundation
+import RxSwift
+import Moya
 
-//class BreweryDBProvider {
+class BreweryDBProvider<T: Codable & DataProtocol> {
+    
+    fileprivate let _dataProvider = MoyaProvider<BreweryDBService<T>>()
+     
+    private func request(requestParams: BreweryDBService<T>) -> Observable<Response<T>> {
+        return self._dataProvider.rx.request(requestParams)
+            .filterSuccessfulStatusAndRedirectCodes()
+            .map(Response<T>.self)
+            .filter({ (val) -> Bool in
+                print("\(requestParams) - \(val.status)") // print requests here
+                return val.status=="success"
+            }).asObservable()
+    }
+
+    func getData(id: String, params: [String:Any]?=nil) -> Observable<Response<T>> {
+        
+        return self.request(requestParams: .getData(id: id, params: params))
+        
+    }
+    
+    func getList(page: Int=1, params: [String:Any]?=nil) -> Observable<Response<T>> {
+        
+        return self.request(requestParams: .getList(page: page, params: params))
+        
+    }
+    
+    func getListFor(id: String, type: DataType, params: [String:Any]?=nil) -> Observable<Response<T>> {
+        
+        return self.request(requestParams: .getListFor(id: id, type: type, params: params))
+        
+    }
+    
+    func search(searchString: String, params: [String:Any]?=nil) -> Observable<Response<T>> {
+        
+        return self.request(requestParams: .search(searchString: searchString, params: params))
+        
+    }
+    
+}
+
+//extension BreweryDBService where T == Brewery {
 //    
-//}
-//
-//extension BreweryDBProvider<T: Codable & DataProtocol> {
-//    
-//}
-//
-//extension BreweryDBService<Brewery> {
+//    func geo(latitude: Float, longitude: Float, radius: Int=100, params: [String:Any]?=nil) -> Observable<Response<T>> {
+//        
+//        self._
+//        
+//    }
 //    
 //}
 
